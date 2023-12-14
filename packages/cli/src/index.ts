251cdfa44f1b4fe6485a20c0d59726e75b5e4a91
ro-mgh <romyagchenkov@gmail.com>
2023-12-14 20:10:26 +0300

    Bump cli version

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ function main() {
   program
     .name('mallaui')
     .description('add components and dependencies to your react native project')
-    .version('0.0.4');
+    .version('0.0.5');
 
   program.addCommand(init).addCommand(add);
 
